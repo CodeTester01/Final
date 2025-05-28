8d3bbff628391eba4029501467c61cdc32414a2a
CodeTester01 <amansingh351998@gmail.com>
2025-05-28 23:27:55 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/example/agri_delivery_app/MainActivity.java
+++ android/app/src/main/java/com/example/agri_delivery_app/MainActivity.java
@@ -1,6 +1,11 @@
-package com.example.agri_delivery_app;
-
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
 }
